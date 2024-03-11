08a36a977724d8d8af842f107725a0990e789794
bubbaboogs <Karen@DESKTOP-CV1DBR4.attlocal.net>
2024-03-11 17:01:52 -0400

    bomb

---
+++ src/main/java/net/mcreator/bandbadditions/init/BandbAdditionsModPaintings.java
@@ -0,0 +1,18 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.bandbadditions.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.world.entity.decoration.PaintingVariant;
+
+import net.mcreator.bandbadditions.BandbAdditionsMod;
+
+public class BandbAdditionsModPaintings {
+	public static final DeferredRegister<PaintingVariant> REGISTRY = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, BandbAdditionsMod.MODID);
+	public static final RegistryObject<PaintingVariant> BOMB = REGISTRY.register("bomb", () -> new PaintingVariant(32, 32));
+}
